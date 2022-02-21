1d4e89dd140089b5bf237dc47ba3708a9e29b506
Claire H <50155045+clairejhaas@users.noreply.github.com>
2022-02-21 03:23:02 +0000

    changed links

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import time
+
 print("")
 print("------------------------------------------------------ ")
-print("Welcome to the Humanities of the Early World text game! ") 
+print("Welcome to the Humanities of the Early World text game! ")
 time.sleep(1)
-print("Note: game works best on a full screen. ") 
+print("Note: game works best on a full screen. ")
 time.sleep(1)
 print("You will read a variety of prompts which replicate the Ancient World. ")
 time.sleep(1)
@@ -11,7 +12,7 @@ print("You will have to make a series of decisions based on these prompts. ")
 time.sleep(1)
 print("Based on historical research (sources provided), you will receive a factual outcome of your decision. ")
 time.sleep(1)
-print("View sources on github at 'https://github.com/clairehaaas/AncientWorldSurvivalGame'. Designed by @clairehaaas. ") 
+print("View sources on github at 'https://github.com/clairejhaas/AncientWorldSurvivalGame'. Designed by @clairejhaas. ")
 time.sleep(1)
 print("You will start with 40 health points (points are awarded for correct responses). ")
 time.sleep(1)
@@ -23,27 +24,24 @@ time.sleep(3)
 
 hp = 40
 
-def get_input(prompt, accepted):
-  while True:
-      value = input(prompt).lower()
 
-      if value in accepted:
-        return value
-      else:
-        print("Sorry, but your answer is not a recognized answer. Please type one of ", accepted)
+def get_input(prompt, accepted):
+    while True:
+        value = input(prompt).lower()
+
+        if value in accepted:
+            return value
+        else:
+            print("Sorry, but your answer is not a recognized answer. Please type one of ", accepted)
+
+
 print("Would you like to hear modern day connections about the Ancient World? ")
-commentary = get_input("Yes or no? ",["yes","no"])
+commentary = get_input("Yes or no? ", ["yes", "no"])
 print("")
 
-def commentaryModeOn():
-    print ("Commentary mode: on.")
-
-def commentaryModeOff():
-    print ("Commentary mode: off.")
-
 if commentary == "yes":
-  commentaryModeOn()
-  exec(open("commentary.py").read())
+    print("Commentary mode: on.")
+    exec(open("game.py").read(), {"with_commentary": True, "hp": hp})
 elif commentary == "no":
-  commentaryModeOff()
-  exec(open("nocommentary.py").read())
+    print("Commentary mode: off.")
+    exec(open("game.py").read(), {"with_commentary": False, "hp": hp})
